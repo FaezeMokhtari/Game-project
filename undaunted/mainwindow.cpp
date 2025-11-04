73c775e636cda79ae912f1bfbb46c986b280ece7
882-fateme <f07891858@gmail.com>
2025-11-05 00:42:03 +0330

    Create mainwindow.cpp

---
+++ undaunted/mainwindow.cpp
@@ -0,0 +1,45 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QString>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    this->showMaximized();
+
+
+    ui->wellcome->setStyleSheet("background-image: url(:/new/prefix1/w.png);"
+                                "background-repeat: no-repeat;"
+                                "background-position: center;"
+                                "background-size: cover;");
+
+
+    ui->page->setStyleSheet("background-image: url(:/new/prefix2/sec.png);"
+                             "background-repeat: no-repeat;"
+                             "background-position: center;"
+                             "background-size: cover;");
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_pushButton_1_clicked()
+{
+    QString name1 = ui->lineEdit_2->text();
+    QString name2 = ui->lineEdit->text();
+    //player p1(name1);
+    //player p2(name2);
+
+    ui->stackedWidget->setCurrentIndex(2);
+    // ui->label_9->setText(name1);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
