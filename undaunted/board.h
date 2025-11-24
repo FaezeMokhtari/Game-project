853a0c52e82ba686305adb28cc75bb95255b688c
882-fateme <f07891858@gmail.com>
2025-11-24 20:40:26 +0330

    Update board.h

--- undaunted/board.h
+++ undaunted/board.h
@@ -1,19 +1,22 @@
 #ifndef BOARD_H
 #define BOARD_H
-#include <vector>
-#include "rectangle.h"
-#include <string>
 
-using namespace std;
+#include "rectangle.h"
+#include "cell.h"
+#include <QGraphicsScene>
+#include <QVector>
+#include <QString>
 
 class board
 {
-    vector <rectangle*> R;
-
 public:
-    board();
-    rectangle& pars(const string& b);
+    QVector<rectangle*> R;
+    QGraphicsScene* scene;
 
+    board();
+    ~board();
+    QVector<cell*> pars(const QString& mapText);
+    void graphic(QGraphicsScene* scene, QVector<cell*> grid);
 };
 
 #endif // BOARD_H
