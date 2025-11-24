c4cc47ff15548fb31167a04acfe851c5f8a6ebab
882-fateme <f07891858@gmail.com>
2025-11-24 03:44:44 +0330

    Update board.h

--- undaunted/board.h
+++ undaunted/board.h
@@ -1,10 +1,19 @@
 #ifndef BOARD_H
 #define BOARD_H
+#include <vector>
+#include "rectangle.h"
+#include <string>
+
+using namespace std;
 
 class board
 {
+    vector <rectangle*> R;
+
 public:
     board();
+    rectangle& pars(const string& b);
+
 };
 
 #endif // BOARD_H
