eaad17b39e8667e400b7d83c275641cbbc2f2e58
882-fateme <f07891858@gmail.com>
2025-11-24 20:40:45 +0330

    Update rectangle.h

--- undaunted/rectangle.h
+++ undaunted/rectangle.h
@@ -1,15 +1,19 @@
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
 
+#include <QGraphicsPolygonItem>
+#include <QBrush>
+#include <QPen>
+
 class rectangle
 {
+public:
     int n;
     int x;
     int y;
     int s;
     char r;
 
-public:
     rectangle(int nn, int nx, int ny, int ns, char nr);
     friend class board;
 };
