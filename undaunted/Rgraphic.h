05ef52962a8f5b53d6431c800ec88a905744af83
882-fateme <f07891858@gmail.com>
2025-12-13 21:50:15 +0330

    Update Rgraphic.h

--- undaunted/Rgraphic.h
+++ undaunted/Rgraphic.h
@@ -12,17 +12,18 @@ class Rgraphic : public QGraphicsPathItem
 public:
     explicit Rgraphic(rectangle* model, QGraphicsItem* parent = nullptr);
 
+    void setw(float w){width = w;};
+    void seth(float h){height = h;};
     void setBackground(const QPixmap& pixmap, const QRect& sourceRect);
-
     void updateAppearance();
 
 private:
     rectangle* model;
     QBrush backgroundBrush;
     QPen borderPen;
-    float width = 90;
-    float height = 120;
-    float cornerRadius = 10;
+    float width = 60;
+    float height = 70;
+    float cornerRadius = 5;
 };
 
 #endif // RGRAPHIC_H
