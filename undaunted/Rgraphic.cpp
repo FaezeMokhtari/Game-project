283eed50514ec6add898d26e03a0d9d3afd23846
882-fateme <f07891858@gmail.com>
2025-12-15 01:13:24 +0330

    Update Rgraphic.cpp

--- undaunted/Rgraphic.cpp
+++ undaunted/Rgraphic.cpp
@@ -1,40 +1,55 @@
 #include "Rgraphic.h"
 #include <QPainter>
-#include "rectangle.h"
-#include <QGraphicsPathItem>
 
 Rgraphic::Rgraphic(rectangle* m, QGraphicsItem* parent)
     : QGraphicsPathItem(parent), model(m)
 {
-    QPainterPath path;
-    path.addRoundedRect(0, 0, width, height, cornerRadius, cornerRadius);
-    setPath(path);
-
     borderPen = QPen(Qt::black, 2);
     setPen(borderPen);
 
     updateAppearance();
-
-    float spacing = 3;
-    float xPos = model->x * (width + spacing);
-    float yPos = model->y * (height + spacing);
-
-    if (model->y % 2 != 0) xPos += (width + spacing) / 2.0f;
-
-    setPos(xPos, yPos);
     setZValue(1);
+}
+
+
+void Rgraphic::setw(float w)
+{
+    width = w;
+    updateAppearance();
+}
+
+
+void Rgraphic::seth(float h)
+{
+    height = h;
+    updateAppearance();
 }
 
 void Rgraphic::updateAppearance()
 {
-    setBrush(Qt::darkGray);
+    prepareGeometryChange();
 
-    if (!backgroundBrush.texture().isNull()) setBrush(backgroundBrush);
+    QPainterPath path;
+    path.addRoundedRect(0, 0, width, height, cornerRadius, cornerRadius);
+    setPath(path);
+
+    if (!backgroundBrush.texture().isNull())
+        setBrush(backgroundBrush);
+    else
+        setBrush(Qt::darkGray);
 }
 
 void Rgraphic::setBackground(const QPixmap& pixmap, const QRect& sourceRect)
 {
     QPixmap cropped = pixmap.copy(sourceRect);
-    backgroundBrush = QBrush(cropped);
+
+    QPixmap scaled = cropped.scaled(
+        width,
+        height,
+        Qt::IgnoreAspectRatio,
+        Qt::SmoothTransformation
+        );
+
+    backgroundBrush = QBrush(scaled);
     updateAppearance();
 }
